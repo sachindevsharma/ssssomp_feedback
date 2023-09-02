1b6ce7501737ce3b0d3429abb2eeafee7c921013
Sachin Dev Sharma <sachindevsharma9@gmail.com>
2023-09-02 11:50:31 +0530

    add secrets

--- mongo_connector.py
+++ mongo_connector.py
@@ -1,5 +1,6 @@
 from pymongo import MongoClient
 import os
+import json
 
 
 class MongoConnector:
@@ -8,10 +9,14 @@ class MongoConnector:
         USERNAME = os.environ.get("USERNAME", None)
         PASSWORD = os.environ.get("PASSWORD", None)
 
-        if USERNAME is None:
-            MONGO_URL = open("url.txt", "r").read()        # FOR LOCAL DEVELOPMENT
-        url = f"mongodb+srv://{USERNAME}:{PASSWORD}@ssssomp.pbugdaw.mongodb.net/?retryWrites=true&w=majority"
-        client = MongoClient("MONGO_URL")
+        if USERNAME is None or PASSWORD is None:
+            secrets = json.load(open("url.txt", "r"))      # FOR LOCAL DEVELOPMENT
+            USERNAME = secrets.get("USERNAME")
+            PASSWORD = secrets.get("PASSWORD")
+
+        MONGO_URL = f"mongodb+srv://{USERNAME}:{PASSWORD}"
+        MONGO_URL += "@ssssomp.pbugdaw.mongodb.net/?retryWrites=true&w=majority"
+        client = MongoClient(MONGO_URL)
         return client
     
     def get_database(self, db):
